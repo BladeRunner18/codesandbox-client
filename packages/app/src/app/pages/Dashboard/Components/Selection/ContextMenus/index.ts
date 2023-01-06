0682d839a03a03935425447a21129bdf01f657e6
魏明 <ming.wei@weimob.com>
2023-01-06 11:10:12 +0800

    delete file

--- packages/app/src/app/pages/Dashboard/Components/Selection/ContextMenus/index.ts
+++
@@ -1,8 +0,0 @@
-export { MultiMenu } from './MultiItemMenu';
-export { SandboxMenu } from './SandboxMenu';
-export { FolderMenu } from './FolderMenu';
-export { RepoMenu } from './RepoMenu';
-export { MasterMenu } from './MasterMenu';
-export { ContainerMenu } from './ContainerMenu';
-export { RepositoryMenu } from './RepositoryMenu';
-export { CommunitySandboxMenu } from './CommunitySandboxMenu';
--- packages/app/src/app/pages/Dashboard/Components/Skeleton/index.ts
+++
@@ -1,2 +0,0 @@
-export { DefaultSkeleton } from './DefaultSkeleton';
-export { SolidSkeleton } from './SolidSkeleton';
